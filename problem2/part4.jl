afcf2733cc20fcfb680061ac3e532a30433c6dbf
Timothy Wright <timmyytjr@gmail.com>
2026-04-02 02:35:50 -0400

    Formatting for LeTeX

--- problem2/part4.jl
+++ problem2/part4.jl
@@ -1,6 +1,5 @@
 using Plots
 
-# TODO: Put Parameters here
 B = 15
 A = 10
 omega = pi/12
@@ -8,7 +7,6 @@ phi = -((3*pi)/4)
 k = 0.4
 T0 = 85
 
-# TODO: Put found Solution here
 f(t, T) = k*(A*cos(omega*t + phi) + B) - k*T
 
 # Create grid for slope field
@@ -21,17 +19,17 @@ T_grid = [T for t in trange, T in Trange]
 u = ones(size(t_grid)) # Horizontal component
 v = f.(t_grid, T_grid) # Vertical component (slope)
 
-# TODO: Particular solution goes here
-sol(t) = 17.64*(cos(omega*t - phi) + omega*sin(omega*t - phi)) + B + 85.64*exp(-k*t)
+sol(t) = 17.64*(cos(omega*t - phi) + omega*sin(omega*t - phi)) 
+       + B + 85.64*exp(-k*t)
+
 t_fine = 0:0.1:60
 
-# TODO: Change title as needed
-quiver(t_grid, T_grid, quiver=(u, v), arrow=arrow(:closed, :head, 0.1, 0.1), 
+quiver(t_grid, T_grid, quiver=(u, v), 
+       arrow=arrow(:closed, :head, 0.1, 0.1), 
        title="Coffee Outside in Boston", 
        xlabel="Time (Hours)", ylabel="Temp (C)", 
        color=:gray, alpha=0.5, label="Slope Field")
 
-# TODO: Change title as needed. This is for Particular solution
 p = plot!(t_fine, sol.(t_fine), color=:red, linewidth=3, 
        label="Particular Solution")
 
